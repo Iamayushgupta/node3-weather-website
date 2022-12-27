0446cdbe15cf8e4061c952a9e322e95e5aa42f56
Ayush <lionelayush446@gmail.com>
2022-12-27 16:22:44 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#msg-1')
+const msg2 = document.querySelector('#msg-2')
+weatherForm.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+    msg1.textContent="Loading..."
+    const location = search.value
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+    response.json().then(({error,location,temperature})=>{
+        if(error){
+            msg1.textContent=error
+        }
+        else{
+            msg1.textContent=location
+            msg2.textContent="The temperature here is " + temperature +" degrees"
+        }
+    })
+})
+})
